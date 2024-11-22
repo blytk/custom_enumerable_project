babb1ee8247d92676e0cfb96da73d2a1c5c121aa
Yago <dolmayan@gmail.com>
2024-11-22 14:03:05 +0100

    Create first custom enumerable methods (my_each and my_each_with_index) for arrays

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,15 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    if block_given?
+      i = 0
+      self.my_each do |element|
+        yield [element, i]
+        i = i + 1
+      end
+      self
+    end
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +18,15 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    # iterate through all elements of the array
+    for i in 0...self.length
+      # current element
+      current_element = self[i]
+      # yield current element to block
+      yield current_element
+    end
+    # return original array
+    self
+  end
 end
